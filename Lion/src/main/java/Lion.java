7be4678159aba00cf1ee7254fc8be433317eb20d
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2019-05-14 11:20:09 -0500

    Number of lions created + lions max age

--- Lion/src/main/java/Lion.java
+++ Lion/src/main/java/Lion.java
@@ -1,97 +1,101 @@
 public class Lion {
-
-
-//    methods
+    public static int numberOfLionsCreated = 0;
+    public static int lionMaxAge = 0;
 
     private String sex;
     private int age;
     private String name;
     private boolean hungry;
 
-    //    Constructor
     public Lion(String sex, int age, String name, boolean hungry) {
         this.sex = sex;
         this.age = age;
         this.name = name;
         this.hungry = hungry;
+
+        if (age > lionMaxAge) {
+            lionMaxAge = age;
+        }
+        numberOfLionsCreated++;
     }
 
+    public Lion(String name) {
+        this.name = name;
 
+        numberOfLionsCreated++;
+    }
 
+    public boolean eat(String food) {
+        if (food.equals("ant")) {
+            hungry = true;
+        } else if (food.equals("antelope") && this.hungry == true) {
+            hungry = false;
+        }
+        return hungry;
+    }
+
+    public void run(String speed) {
+        System.out.println("The lion is running " + speed);
+    }
+
+    public void roar() {
+        System.out.println("ROOOOOOOOOOAR");
+    }
+
+//    public Lion(String sex) {
+//        this.sex = sex;
+//        this.age = 0;
+//        this.name = "Jeff";
+//        this.hungry = false;
+//    }
+//
 //    public Lion() {
-//        this.sex = "Male";
+//        this.sex = "male";
 //        this.age = 0;
 //        this.name = "Jeff";
 //        this.hungry = false;
 //    }
 
-
-    public String getSex() {
-        return sex;
-    }
-
-
-    public void setsex(String sex) {
-        if (sex.equals("Male") || sex.equals("female")) {
-            System.out.println("Please use male or female to set sex");
-        } else {
-            this.sex = sex;
-        }
-
-    }
-
-//    private void setsex(String sex) {
-//        if(!sex.equals("Male") && !sex.equals("female")) {
-//            System.out.println("Please use male or female to set sex");
-//        } else {
-//            this.sex = sex;
-//        }
-//
-//    }
-
-    public int getAge() {
-        return age;
-
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public boolean eat(String food) {
-        if (food.equals("Ant")) {
-            hungry = true;
-
-        } else if (food.equals("antelope") && this.hungry == true) {
-            hungry = false;
-
-        }
-        return hungry;
-    }
-
-
-    public void run(String speed) {
-        System.out.println("The lion is running " + run);
-    }
-
-    public void roar() {
-        System.out.println("ROAR");
-    }
-
-    public boolean isHungry() {
+    public boolean getHungry() {
         return hungry;
     }
 
     public void setHungry(boolean hungry) {
         this.hungry = hungry;
     }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        if (sex.equals("male") || sex.equals("female")) {
+            this.sex = sex;
+        } else {
+            System.out.println("Please use male or female to set sex");
+        }
+
+//        if (!sex.equals("male") && !sex.equals("female")) {
+//            System.out.println("Please use male or female to set sex");
+//        } else {
+//            this.sex = sex;
+//        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+
+        if (age > lionMaxAge) {
+            lionMaxAge = age;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
